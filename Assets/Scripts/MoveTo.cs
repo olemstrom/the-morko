5803d594961e39d27af5cfca85c088040c560e03
ttjkinnunen <ttjkinnunen@gmail.com>
2015-11-27 12:20:55 +0200

    Mörköupdate
    Nyt se ampuu lasereita ja jahtaa pelaajaa

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -4,37 +4,55 @@ using System.Collections;
 public class MoveTo : MonoBehaviour 
 {
 	public Transform goal;
+	public Transform rayOrig;
+	public Transform playerTargetPoint;
+
 	private bool chasePlayed;
 	public int aggroDistance;
 	public int turboMörkö;
 	public AudioClip murina;
 	public GameObject musicMgr;
 
+	private NavMeshAgent agent;
+	private Vector3 distance_v;
+
 	void Start () 
 	{
 		GetComponent<AudioSource> ().clip = murina;
-		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent = GetComponent<NavMeshAgent> ();
 		agent.destination = goal.position;
 		chasePlayed = false;
+		distance_v = new Vector3();
 	}
 
 	void Update()
 	{
-		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
 		agent.destination = goal.position;
 
-		if (Vector3.Distance(agent.transform.position, goal.position) <= aggroDistance && !chasePlayed){
+		if(playerIsAggroable()){
+			Debug.Log ("Player in sight!");
 			GetComponent<AudioSource>().Play();
 			musicMgr.GetComponent<MusicManager>().playChase();
 			chasePlayed = true;
 			agent.speed = turboMörkö;
-		}
+		} 
 
-		if (Vector3.Distance (agent.transform.position, goal.position) > aggroDistance && chasePlayed) {
+		if(Physics.Linecast (rayOrig.position, playerTargetPoint.position) && chasePlayed){
+			Debug.Log ("Player lost");
+			Debug.DrawLine (rayOrig.position, playerTargetPoint.position, Color.red);
 			chasePlayed = false;
-			agent.speed = 2; // perusnopeus
+			agent.speed = 4; // perusnopeus
 			musicMgr.GetComponent<MusicManager>().playNormal ();
-			//Pitäskö tää viel fade in?
+		}
+	}
+
+	// Testaa näkeekö mörkö pelaajan ja onko etäisyys tarpeeksi pieni
+	private bool playerIsAggroable()
+	{
+		if(!Physics.Linecast (rayOrig.position, playerTargetPoint.position) && !chasePlayed && Vector3.Distance(rayOrig.position, goal.position) <= aggroDistance){
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
