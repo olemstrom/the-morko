dda753f7b1c25689d6dceb8b6a7ff4e20b587966
ttjkinnunen <ttjkinnunen@gmail.com>
2015-12-09 19:46:20 +0200

    Menu done
    Mainmenu versio 0.01a-rc4-build23412

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Button playButton;
+	public Button quitButton;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		playButton = playButton.GetComponent<Button>();
+		quitButton = quitButton.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel(1);
+	}
+}
