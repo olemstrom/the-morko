3da9ac6bb79710a9aa990f152ddee954792b9b73
ttjkinnunen <ttjkinnunen@gmail.com>
2015-12-09 23:18:48 +0200

    Pelaajan voitto ja hävijö skriptit
    Skriptit tehty. Viel pitää lisää mahdolliset scenet tms mitkä ne
    triggeraa

---
+++ Assets/Scripts/PlayerLose.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerLose : MonoBehaviour
+{
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.name == "Player"){
+			Debug.Log("MÖRKÖ SAI PELAAJAN KIINNI");
+		}
+	}
+}
+
---
+++ Assets/Scripts/PlayerWin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWin : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.name == "Player" &&  GlobalVariables.keyFound()){
+			Debug.Log ("PLAYER WIN");
+		}
+	}
+}
