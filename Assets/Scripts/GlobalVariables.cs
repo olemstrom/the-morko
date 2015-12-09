01d016675c4abc1e8f00e84c172bf636c4e892c4
ttjkinnunen <ttjkinnunen@gmail.com>
2015-12-10 01:33:15 +0200

    Korjailuja

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -13,6 +13,5 @@ public static class GlobalVariables
 		else
 			return false;
 	}
-
 }
 
--- Assets/Scripts/KeyItem.cs
+++ Assets/Scripts/KeyItem.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class KeyItem : MonoBehaviour
 {
+	public Texture aTexture;
+	private Texture tmp;
+
 	void OnTriggerEnter(Collider collider)
 	{
-
 		if(collider.gameObject.name == "Player"){
-			Destroy (gameObject);
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+			gameObject.GetComponent<MeshCollider>().enabled = false;
 			GlobalVariables.keyCount++;
 		}
+	}
+
+	void Start()
+	{
+		tmp = aTexture;
+		aTexture = null;
+	}
+
+	void Update()
+	{
+		if(GlobalVariables.keyFound ())
+			aTexture = tmp;
+	}
+
+	void OnGUI()
+	{
+		//Piirretään täs tyhjää kunnes avain löytyy ja tekstuuriks asetetaan tmpst poimittu avain
+		GUI.DrawTexture(new Rect(10, 10, 60, 60), aTexture, ScaleMode.StretchToFill, true, 10.0F);
 	}
 }
 
